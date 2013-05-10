241432a6fb1fad143111d205e48c73f267ec6625
Chris Wilson <cwilso@gmail.com>
2013-05-09 17:01:29 -0700

    Audiodisplay now scrolls past 32k

--- js/audiodisplay.js
+++ js/audiodisplay.js
@@ -28,44 +28,67 @@ var SECONDS_OF_RUNNING_DISPLAY = 2.0;
 
 function drawRunningDisplay( context, cache, centerInSeconds ) {
     var center = Math.floor( centerInSeconds * RUNNING_DISPLAY_WIDTH / SECONDS_OF_RUNNING_DISPLAY );
+//    var centerOffset = RUNNING_DISPLAY_HALF_WIDTH - center;
+    var left = center - RUNNING_DISPLAY_HALF_WIDTH;
 
-    context.drawImage( cache.canvas, RUNNING_DISPLAY_HALF_WIDTH - center, 0 );
+    var leftEdgeIndex = Math.floor((center - RUNNING_DISPLAY_HALF_WIDTH)/MAX_CANVAS_WIDTH);
+    if (leftEdgeIndex<0)
+        leftEdgeIndex=0;
+    var rightEdgeIndex = Math.floor((center + RUNNING_DISPLAY_HALF_WIDTH)/MAX_CANVAS_WIDTH);
+    if (rightEdgeIndex>=cache.length)
+        rightEdgeIndex = cache.length - 1;
+
+//    if (center!=0)
+//        console.log("draw: " + leftEdgeIndex + ":" + rightEdgeIndex);
+    for (var i = leftEdgeIndex; i<=rightEdgeIndex; i++) {
+        context.drawImage( cache[i], RUNNING_DISPLAY_HALF_WIDTH - center + (MAX_CANVAS_WIDTH*i), 0 );
+//        if (center!=0)
+//            console.log("cache " + i + " offset " + (RUNNING_DISPLAY_HALF_WIDTH - center + (MAX_CANVAS_WIDTH*i)));
+    }
 }
+
+var MAX_CANVAS_WIDTH = 32000;   // limitation - can't draw canvases wider than 32k
 
 function createRunningDisplayCache( buffer, isTop ) {
     var step = SECONDS_OF_RUNNING_DISPLAY * buffer.sampleRate / RUNNING_DISPLAY_WIDTH;
     var newLength = Math.floor( buffer.duration / SECONDS_OF_RUNNING_DISPLAY * RUNNING_DISPLAY_WIDTH );
     var data = buffer.getChannelData(0);
+    var numCanvases = Math.ceil( newLength / MAX_CANVAS_WIDTH );
+    var canvases = [];
 
-    newLength = Math.min(newLength, 32000   );
-
-    // create canvas with width of the reduced-in-size buffer's length.
-    var canvas = document.createElement('canvas');
-    canvas.width = newLength;
-    canvas.height = RUNNING_DISPLAY_HEIGHT;  
-    var drawCTX = canvas.getContext('2d');
-
-    drawCTX.clearRect(0,0,newLength,RUNNING_DISPLAY_HEIGHT);
-
-    // set the color for the waveform display
-    drawCTX.fillStyle = isTop ? "blue" : "red";
+//    newLength = Math.min(newLength, 32000   );
 
     // draw the canvas
-    for (var i=0; i<newLength; i++) {
-        var max = 0.0;
-        var offset = Math.floor(i*step);
-        for (var j=0; j<step; j++) {
-            var datum = data[offset+j];
-            if (datum < 0)
-                datum = -datum;
-            if (datum > max)
-                max = datum;
+    for (var j=0; j<newLength; j+=MAX_CANVAS_WIDTH) {
+        // create canvas with width of the reduced-in-size buffer's length.
+        var canvas = document.createElement('canvas');
+        var width = newLength - j;
+        canvas.width = (width>MAX_CANVAS_WIDTH) ? MAX_CANVAS_WIDTH : width;
+        canvas.height = RUNNING_DISPLAY_HEIGHT;  
+        var drawCTX = canvas.getContext('2d');
+
+        drawCTX.clearRect(0,0,newLength,RUNNING_DISPLAY_HEIGHT);
+        // set the color for the waveform display
+        drawCTX.fillStyle = isTop ? "blue" : "red";
+
+        // draw the canvas
+        for (var i=0; i<width; i++) {
+            var max = 0.0;
+            var offset = Math.floor((i+j)*step);
+            for (var k=0; k<step; k++) {
+                var datum = data[offset+k];
+                if (datum < 0)
+                    datum = -datum;
+                if (datum > max)
+                    max = datum;
+            }
+            max = Math.floor( max * RUNNING_DISPLAY_HEIGHT / 2 );
+            if (isTop)
+                drawCTX.fillRect(i,0,1,max);
+              else
+                drawCTX.fillRect(i,RUNNING_DISPLAY_HEIGHT,1,-max);
         }
-        max = Math.floor( max * RUNNING_DISPLAY_HEIGHT / 2 );
-        if (isTop)
-            drawCTX.fillRect(i,0,1,max);
-          else
-            drawCTX.fillRect(i,RUNNING_DISPLAY_HEIGHT,1,-max);
+        canvases.push( canvas );
     }
-    return { canvas: canvas, context: drawCTX };
+    return canvases;
 }
